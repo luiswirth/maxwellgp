286e602a7ecb3c769d303c631a84a30c28e07e21
Luis Wirth <lwirth2000@gmail.com>
2026-06-01 14:47:18 +0200

    add pyright; fix package __init__.py

--- src/maxwellgp/__init__.py
+++ src/maxwellgp/__init__.py
@@ -1,11 +1,9 @@
 from maxwellgp.gp import GaussianProcess
 from maxwellgp.kernel import (
     FullMaxwellKernel,
-    TangentialMaxwellKernel,
 )
 
 __all__ = [
     "GaussianProcess",
     "FullMaxwellKernel",
-    "TangentialMaxwellKernel",
 ]
